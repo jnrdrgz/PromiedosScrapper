71e5c852958fbc65cca4fb3227c8916ad40431c6
jnrdrgz <jnigrodriguez@gmail.com>
2019-02-07 18:55:22 -0300

    prueba username

--- examples/__Init__.py
+++ examples/__Init__.py
@@ -43,6 +43,7 @@ should return:
 ["team", "team", "team", "team" ... "score home team", "score away team"]
 
 example:
-'Talleres (C)', 'Rosario Central', 'Newells', 'Independiente', 'Def y Justicia', 'Atl Tucuman', 'Banfield', 'Gimnasia LP', 'Argentinos', 'Godoy Cruz', 'River Plate', 'Belgrano', 'San Martin (SJ)', 'Patronato', 'Colon', 'Tigre', 'Aldosivi', 'Huracan', 'San Lorenzo', 'Lanus', 'Racing Club', 'Velez', 'Estudiantes LP', 'Boca Juniors', 'San Martin (T)', 'Union', '0', '1', '2', '2', '1', '1', '1', '0', '0', '0', '0', '0', '1', '0', '2', '2', '2', '1', '2', '2', '2', '0', '2', '0', '1', '1']
+'Talleres (C)', 'Rosario Central', 'Newells', 'Independiente', 'Def y Justicia', 'Atl Tucuman', 'Banfield', 'Gimnasia LP', 'Argentinos', 'Godoy Cruz', 'River Plate', 'Belgrano', 'San Martin (SJ)', 'Patronato', 'Colon', 'Tigre', 'Aldosivi', 'Huracan', 'San Lorenzo', 'Lanus', 'Racing Club', 'Velez', 'Estudiantes LP', 'Boca Juniors', 'San Martin (T)', 'Union', 
+'0', '1', '2', '2', '1', '1', '1', '0', '0', '0', '0', '0', '1', '0', '2', '2', '2', '1', '2', '2', '2', '0', '2', '0', '1', '1']
 
 '''
